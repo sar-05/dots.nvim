7f7746b14b2de180ce164b5356bef2afe5cf0a86
Sebastian Alighieri <sebastian.alighierir@uanl.edu.mx>
2025-11-13 11:46:28 -0600

    feat: tweak configuration and add plugins
    Setup the LSPs for the languages I use.
    Enable support for Nerd Fonts
    Disable support for unused providers.
    Remove legacy code for Neovim < 11.0
    Add after configurations for markdown and gitcommit filetypes.
    Add git linter.
    Add and set gruvbox-material colorscheme.
    Disable arrow keys in normal mode and mouse support.
    Add delay for which-key.

---
+++ lua/custom/plugins/gruvbox-material.lua
@@ -0,0 +1,11 @@
+return {
+  'sainnhe/gruvbox-material',
+  priority = 1000,
+  -- "ellisonleao/gruvbox.nvim",
+  -- config = function()
+  --   require("gruvbox").setup({
+  --     transparent_mode = true
+  --   })
+  --   vim.cmd("colorscheme gruvbox")
+  -- end
+}
