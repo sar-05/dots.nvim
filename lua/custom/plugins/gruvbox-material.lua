cb7629297d7c62cab789ffe9698e1c097f1f74e2
Sebastian Alighieri <sebastian.alighierir@uanl.edu.mx>
2025-11-24 11:20:18 -0600

    Add harpoon plugin

--- lua/custom/plugins/gruvbox-material.lua
+++ lua/custom/plugins/gruvbox-material.lua
@@ -1,11 +1,4 @@
 return {
   'sainnhe/gruvbox-material',
   priority = 1000,
-  -- "ellisonleao/gruvbox.nvim",
-  -- config = function()
-  --   require("gruvbox").setup({
-  --     transparent_mode = true
-  --   })
-  --   vim.cmd("colorscheme gruvbox")
-  -- end
 }
